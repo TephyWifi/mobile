98a8de371a2dda380c5755129c22e2e405e63287
Avantella <ggroenner@gmail.com>
2025-04-21 22:37:28 +0200

    added scrollview to index ^^

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { SafeAreaView, View, Text, Button } from "react-native";
+import { SafeAreaView, View, Text, Button, ScrollView } from "react-native";
 import PackingList from "../../components/PackingList";
 
 const durations = ['Weekend', '1 week', '2+ weeks'] as const
@@ -8,19 +8,21 @@ const Packing = () => {
   const [selectedDuration, setSelectedDuration] = useState<typeof durations[number]>('Weekend')
   return (
     <SafeAreaView style={{ padding: 20 }}>
-      <Text style={{ fontSize: 18, fontWeight: 'bold', marginBottom: 10 }}>Select trip duration:</Text>
-      <View style={{ flexDirection: 'row', marginBottom: 20, }}>
-        {durations.map((duration) =>(
-          <Button
-            key={duration}
-            title={duration}
-            onPress={() => setSelectedDuration(duration)}
-            color={selectedDuration === duration ? '#007AFF' : '#ccc'}
-          />
-        ))}
-      </View>
+      <ScrollView>
+        <Text style={{ fontSize: 18, fontWeight: 'bold', marginBottom: 10 }}>Select trip duration:</Text>
+        <View style={{ flexDirection: 'row', marginBottom: 20, }}>
+          {durations.map((duration) => (
+            <Button
+              key={duration}
+              title={duration}
+              onPress={() => setSelectedDuration(duration)}
+              color={selectedDuration === duration ? '#007AFF' : '#ccc'}
+            />
+          ))}
+        </View>
 
-      <PackingList duration={selectedDuration} />
+        <PackingList duration={selectedDuration} />
+      </ScrollView>
     </SafeAreaView>
   )
 }
